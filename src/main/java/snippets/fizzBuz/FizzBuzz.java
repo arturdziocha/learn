6b939cf9138579042b31498bcd86b72e556fb703
arturdziocha <artur.dziocha@gmail.com>
2020-01-31 12:10:47 +0100

    fizz buzz

--- src/main/java/snippets/fizzBuz/FizzBuzz.java
+++ src/main/java/snippets/fizzBuz/FizzBuzz.java
@@ -12,9 +12,9 @@ public class FizzBuzz {
         for (int i = start; i <= end; i++) {
             if (i % 15 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0 && i % 5 != 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0 && i % 3 != 0) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
