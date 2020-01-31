35e7f97e0ed6fa187b58f52cfb02a2fd083eca36
arturdziocha <artur.dziocha@gmail.com>
2020-01-31 14:10:53 +0100

    ss

---
+++ src/main/java/snippets/forLoop/MaxDivisible.java
@@ -0,0 +1,33 @@
+package snippets.forLoop;
+
+import java.util.Scanner;
+
+/*
+ * Given a sequence of natural numbers, not exceeding 30000. Find the maximum
+ * element divisible by 4. The program receives the number of elements in the
+ * sequence and then the elements themselves as input. In the sequence, there is
+ * always an element divisible by 4. The number of elements does not exceed
+ * 1000. The program should print a single number: the maximum element of the
+ * sequence divisible by 4.
+ * 
+ * Sample Input 1: 12 16 87 58 25 73 86 36 79 40 12 89 32
+ */
+
+public class MaxDivisible {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please add numbers");
+        int n = in.nextInt();
+        int currentMax = 0;
+        int num;
+
+        for (int i = 0; i < n; i++) {
+            num = in.nextInt();
+            if (num % 4 == 0 && num > currentMax) {
+                currentMax = num;
+            }
+        }
+        System.out.println(currentMax);
+
+    }
+}
--- src/main/java/snippets/forLoop/ProductOfIntegers.java
+++ src/main/java/snippets/forLoop/ProductOfIntegers.java
@@ -2,15 +2,26 @@ package snippets.forLoop;
 
 import java.util.Scanner;
 
+/*
+ * Write a program that prints the product of all integer numbers from a to b (a
+ * < b). Include a and exclude b from the product.
+ * 
+ * Sample Input 1: 1 2 
+ * Sample Output 1: 1 
+ * Sample Input 2: 100 105 
+ * Sample Output 2: 11035502400
+ */
 public class ProductOfIntegers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("add numbers");
         int start = scanner.nextInt();
         int end = scanner.nextInt();
-        int sum = 1;
-        for (int i = start; i < end; i++) {
-            sum *= i;
+        long sum = 1;
+        for (; start < end; start++) {
+            sum *= start;
         }
         System.out.println(sum);
+        scanner.close();
     }
 }
