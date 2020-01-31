2671232c90fa9f26287ca15c18ccb464450ca214
arturdziocha <artur.dziocha@gmail.com>
2020-01-31 12:14:22 +0100

    ss

--- src/main/java/snippets/fizzBuz/FizzBuzz.java
+++ src/main/java/snippets/fizzBuz/FizzBuzz.java
@@ -9,16 +9,35 @@ public class FizzBuzz {
         System.out.println("Give numbers");
         int start = scanner.nextInt();
         int end = scanner.nextInt();
-        for (int i = start; i <= end; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+        if (start < end) {
+            for (int i = start; i <= end; i++) {
+                if (i % 15 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
             }
+           
+        }
+        else if(start > end) {
+            for (int i = start; i <= end; i--) {
+                if (i % 15 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+        else {
+            //TODO
         }
         scanner.close();
     }
