6c6524b88446873fd8462e3f5edf74a0a5d272b1
arturdziocha <artur.dziocha@gmail.com>
2020-01-31 12:09:22 +0100

    fizz buzz

---
+++ src/main/java/snippets/fizzBuz/FizzBuzz.java
@@ -0,0 +1,25 @@
+package snippets.fizzBuz;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers");
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        for (int i = start; i <= end; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0 && i % 5 != 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0 && i % 3 != 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+        scanner.close();
+    }
+}
