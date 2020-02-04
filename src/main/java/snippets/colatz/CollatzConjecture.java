ba7fac10345f425032559f8685a5f7b76f60c572
arturdziocha <artur.dziocha@gmail.com>
2020-02-04 10:17:55 +0100

    ss

--- src/main/java/snippets/colatz/CollatzConjecture.java
+++ src/main/java/snippets/colatz/CollatzConjecture.java
@@ -13,5 +13,6 @@ public class CollatzConjecture {
             n = (n % 2 == 0) ? (n / 2) : (n * 3 + 1);
         }
         System.out.print(n);
+        scanner.close();
     }
 }
---
+++ src/main/java/snippets/sequence/Sequence.java
@@ -0,0 +1,7 @@
+package snippets.sequence;
+
+public class Sequence {
+    public static void main(String[] args) {
+        
+    }
+}
