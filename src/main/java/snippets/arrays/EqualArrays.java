b24b0a1494407913fe8ea7a174947d18c879afc2
arturdziocha <artur.dziocha@gmail.com>
2020-02-05 22:29:37 +0100

    ss

---
+++ src/main/java/snippets/arrays/EqualArrays.java
@@ -0,0 +1,22 @@
+package snippets.arrays;
+
+import java.util.Arrays;
+
+public class EqualArrays {
+    public static void main(String[] args) {
+        int[] numbers1 = { 1, 2, 3, 4 };
+        int[] numbers2 = { 1, 2, 3, 4 };
+        int[] numbers3 = { 4, 3, 2, 1 };
+        int[] numbers4 = { 1, 2, 3 };
+        boolean e1 = Arrays.equals(numbers1, numbers2);
+        System.out.println(e1);
+        boolean e2 =  Arrays.equals(numbers2, numbers3);
+        System.out.println(e2);
+        boolean e3 =Arrays.equals(numbers1, numbers1);
+        System.out.println(e3);
+        boolean e4 = Arrays.equals(numbers3, numbers1);
+        System.out.println(e4);
+        boolean e5 =Arrays.equals(numbers1, numbers4);
+        System.out.println(e5);
+    }
+}
