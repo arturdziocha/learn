44f3c96c103ff0617104df59a2387db344ad4154
arturdziocha <artur.dziocha@gmail.com>
2020-02-07 11:04:06 +0100

    ss

---
+++ src/main/java/snippets/ternary/Falses.java
@@ -0,0 +1,28 @@
+package snippets.ternary;
+
+public class Falses {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = true;
+        boolean b3 = true;
+        System.out.println((b1 && b2) ? b1 : (b2 || b3) ? b2 : b3);
+        
+        b1 = false;
+        b2 = false;
+        b3 = false;
+        System.out.println((b1 && b2) ? b1 : (b2 || b3) ? b2 : b3);
+        
+        b1 = false;
+        b2 = false;
+        b3 = true;
+        System.out.println((b1 && b2) ? b1 : (b2 || b3) ? b2 : b3);
+        b1 = false;
+        b2 = true;
+        b3 = false;
+        System.out.println((b1 && b2) ? b1 : (b2 || b3) ? b2 : b3);
+        b1 = true;
+        b2 = false;
+        b3 = false;
+        System.out.println((b1 && b2) ? b1 : (b2 || b3) ? b2 : b3);
+    }
+}
