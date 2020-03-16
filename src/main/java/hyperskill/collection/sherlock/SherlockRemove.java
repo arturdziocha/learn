563eb2de9f63a029a4123ea5530aee538bf578b7
arturdziocha <artur.dziocha@gmail.com>
2020-03-16 14:08:33 +0100

    ss

--- src/main/java/hyperskill/collection/sherlock/SherlockRemove.java
+++ src/main/java/hyperskill/collection/sherlock/SherlockRemove.java
@@ -6,9 +6,14 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /*
-For this problem, imagine that you are Sherlock Holmes. You've deduced that the clues are somehow hidden within the pairs of words that contain only the same letters with the same frequencies. To crack the case, you now need to find out how many characters must be deleted to get such words (character sequences) from the given ones.
+For this problem, imagine that you are Sherlock Holmes. 
+You've deduced that the clues are somehow hidden within the pairs of words that contain 
+only the same letters with the same frequencies. 
+To crack the case, you now need to find out how many characters must be deleted to get such words (character sequences) 
+from the given ones.
 
-For example: for two words "case" and "seal" you'll need to remove characters "c" and "l" respectively to get "ase" and "sea". In this case, the answer is 2 ("c" and "l").
+For example: for two words "case" and "seal" you'll need to remove characters "c" and "l" respectively 
+to get "ase" and "sea". In this case, the answer is 2 ("c" and "l").
 
 Remember: these "words" are case-insensitive
 
