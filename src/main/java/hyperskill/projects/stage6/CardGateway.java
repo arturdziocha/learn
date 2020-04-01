30c600bbd309cbb3bbbebf1442dd9e832972dcbe
arturdziocha <artur.dziocha@gmail.com>
2020-04-01 13:53:55 +0200

    ss

--- src/main/java/hyperskill/projects/stage6/CardGateway.java
+++ src/main/java/hyperskill/projects/stage6/CardGateway.java
@@ -2,6 +2,7 @@ package hyperskill.projects.stage6;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 public class CardGateway {
     private List<Card> cards;
@@ -15,7 +16,12 @@ public class CardGateway {
     }
 
     void add(Card card) {
-        //TODO update to check if exists
+        int index = IntStream
+                .range(0, cards.size())
+                .filter(i -> cards.get(i).getName().equals(card.getName()))
+                .findFirst()
+                .orElse(-1);
+        // TODO update to check if exists
         this.cards.add(card);
     }
 
