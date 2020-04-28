d1b9296dcc890caa423a7ed489f00e6082d975f1
arturdziocha <artur.dziocha@gmail.com>
2020-04-28 22:24:43 +0200

    ss

--- src/main/java/hyperskill/projects/tictactoe/stage4/Main.java
+++ src/main/java/hyperskill/projects/tictactoe/stage4/Main.java
@@ -49,7 +49,7 @@ class TicTacToe {
     }
 
     boolean checkNotFinished() {
-        return Arrays.stream(board).flatMap(a -> Arrays.stream(a)).anyMatch(s -> s.equals(BoxState.EMPTY));
+        return Arrays.stream(board).flatMap(Arrays::stream).anyMatch(s -> s.equals(BoxState.EMPTY));
     }
 
     boolean isWin(BoxState state) {
@@ -66,18 +66,14 @@ class TicTacToe {
                     return true;
                 }
             }
-            if (diagonal1 == playerTotal || diagonal2 == playerTotal) {
-                return true;
-            }
-            return false;
+            return diagonal1 == playerTotal || diagonal2 == playerTotal;
         }
         return false;
     }
 
     boolean checkToManyMoves() {
-        long numOfX = Arrays.stream(board).flatMap(a -> Arrays.stream(a)).filter(s -> s.equals(BoxState.X)).count();
-        long numOfO = Arrays.stream(board).flatMap(a -> Arrays.stream(a)).filter(s -> s.equals(BoxState.O)).count();
-        ;
+        long numOfX = Arrays.stream(board).flatMap(Arrays::stream).filter(s -> s.equals(BoxState.X)).count();
+        long numOfO = Arrays.stream(board).flatMap(Arrays::stream).filter(s -> s.equals(BoxState.O)).count();
         return Math.abs(numOfO - numOfX) > 1;
 
     }
