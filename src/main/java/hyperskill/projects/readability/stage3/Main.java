8f6924d8f5bcb459269ac8096d88fc2953d8c5c7
arturdziocha <artur.dziocha@gmail.com>
2020-05-04 22:21:55 +0200

    ss

--- src/main/java/hyperskill/projects/readability/stage3/Main.java
+++ src/main/java/hyperskill/projects/readability/stage3/Main.java
@@ -1,100 +1,46 @@
 package hyperskill.projects.readability.stage3;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("add");
-        // String fileName =
-        // "src/main/java/hyperskill/projects/readability/stage3/test.txt";
-        String fileName = "src/main/java/hyperskill/projects/readability/stage3/" + scanner.nextLine();
+        String fileName = "src/main/java/hyperskill/projects/readability/stage3/test.txt";
+        //String fileName = args[0];
+        String text = readFile(fileName);
+        String[] ages = {"5-6", "6-7", "7-9", "9-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17",
+                "17-18", "18-24", "24+"};
+        long sentences = countWithRegex(text, "[!.?][^$]|$");
+        long words = countWithRegex(text, "\\w+");
+        long chars = countWithRegex(text, "\\S");
+        double score = 4.71 * ((double) chars / words) + 0.5 * ((double) words / sentences) - 21.43;
+        System.out.printf("The text is: \n%s\n\n", text);
+        System.out.printf("Words: %d\n", words);
+        System.out.printf("Sentences: %d\n", sentences);
+        System.out.printf("Characters: %d\n", chars);
+        System.out.printf("The score is: %.2f \n", score);
+        System.out.println(Math.ceil(score) - 1);
+        System.out.printf("This text should be understood by %s year olds.", ages[(int) Math.ceil(score) - 1]);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
-            double numOfChars = 0;
-            StringBuilder builder = new StringBuilder();
+    }
+
+    private static long countWithRegex(String text, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.results().count();
+    }
+
+    private static String readFile(String path) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             int next;
-
             while ((next = reader.read()) != -1) {
                 builder.append((char) next);
-                if (!Character.isWhitespace(next)) {
-                    numOfChars++;
-                }
             }
-            scanner.close();
-            System.out.println("The text is:");
-            System.out.print(builder);
-            List<String> lines = Arrays
-                    .stream(builder.toString().split("[.!?]\\s+"))
-                    .collect(Collectors.toCollection(ArrayList::new));
-            System.out.println();
-            System.out.println();
-            double numOfWords = lines.stream().flatMap(l -> Arrays.stream(l.split("\\s"))).count();
-            System.out.printf("Words: %.0f\n", numOfWords);
-            System.out.printf("Sentences: %d\n", lines.size());
-            System.out.printf("Characters: %.0f\n", numOfChars);
-            double score = 4.71 * (numOfChars / numOfWords) + 0.5 * (numOfWords / lines.size()) - 21.43;
-            int sc = (int) Math.ceil(score);
-            System.out.printf("The score is: %.2f\n", score);
-            String v = "";
-            switch (sc) {
-                case 1:
-                    v = "5-6";
-                    break;
-                case 2:
-                    v = "6-7";
-                    break;
-                case 3:
-                    v = "7-9";
-                    break;
-                case 4:
-                    v = "9-10";
-                    break;
-                case 5:
-                    v = "10-11";
-                    break;
-                case 6:
-                    v = "11-12";
-                    break;
-                case 7:
-                    v = "12-13";
-                    break;
-                case 8:
-                    v = "13-14";
-                    break;
-                case 9:
-                    v = "14-15";
-                    break;
-                case 10:
-                    v = "15-16";
-                    break;
-                case 11:
-                    v = "16-17";
-                    break;
-                case 12:
-                    v = "17-18";
-                    break;
-                case 13:
-                    v = "18-24";
-                    break;
-                case 14:
-                    v = "24+";
-                    break;
-
-                default:
-                    break;
-            }
-            System.out.printf("This text should be understood by %s year olds.", v);
-
         }
-
+        return builder.toString();
     }
 }
