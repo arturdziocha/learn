cf0a654ea649666dabe13a7b9e57e483422eaa44
arturdziocha <artur.dziocha@gmail.com>
2020-05-06 08:57:06 +0200

    # WARNING: head commit changed in the meantime
    ss

--- src/main/java/hyperskill/projects/readability/stage3/Main.java
+++ src/main/java/hyperskill/projects/readability/stage3/Main.java
@@ -13,18 +13,37 @@ public class Main {
         String text = readFile(fileName);
         String[] ages = { "5-6", "6-7", "7-9", "9-10", "10-11", "11-12", "12-13", "13-14", "14-15", "15-16", "16-17",
                 "17-18", "18-24", "24+" };
-        long sentences = countWithRegex(text, "[!.?][^$]|$");
-        long words = countWithRegex(text, "\\w+");
-        long chars = countWithRegex(text, "\\S");
+        long sentences = countWithRegex(text, Regex.SENTECES.getRegex());
+        long words = countWithRegex(text, Regex.WORDS.getRegex());
+        long chars = countWithRegex(text, Regex.CHARS.getRegex());
+        System.out.println(countWithRegex(text, Regex.SYLLABLES.getRegex()));
         double score = 4.71 * ((double) chars / words) + 0.5 * ((double) words / sentences) - 21.43;
-        System.out.printf("The text is: \n%s\n\n", text);
-        System.out.printf("Words: %d\n", words);
-        System.out.printf("Sentences: %d\n", sentences);
-        System.out.printf("Characters: %d\n", chars);
-        System.out.printf("The score is: %.2f \n", score);
-        System.out.println(Math.ceil(score) - 1);
-        System.out.printf("This text should be understood by %s year olds.", ages[(int) Math.ceil(score) - 1]);
+        print(text, words, sentences, chars, score, ages);
 
+    }
+
+    public static void print(String text, long words, long sentences, long chars, double score, String[] ages) {
+        System.out
+                .printf(
+                    "The text is: \n%s\n\n" + "Words: %d\n" + "Sentences: %d\n" + "Characters: %d\n"
+                            + "The score is: %.2f\n" + "This text should be understood by %s year olds.",
+                    text, words, sentences, chars, score, ages[(int) Math.ceil(score) - 1]);
+    }
+
+    enum Regex {
+        SENTECES("[!.?][^$]|$"),
+        WORDS("\\w+"),
+        CHARS("\\S"),
+        SYLLABLES("(?i)[aiouy][aeiouy]*|e[aeiouy]*(?!d?\\b)");
+        private final String regex;
+
+        private Regex(String regex) {
+            this.regex = regex;
+        }
+
+        public String getRegex() {
+            return regex;
+        }
     }
 
     private static long countWithRegex(String text, String regex) {
---
+++ src/main/java/hyperskill/projects/readability/stage4/Main.java
@@ -0,0 +1,10 @@
+package hyperskill.projects.readability.stage4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
