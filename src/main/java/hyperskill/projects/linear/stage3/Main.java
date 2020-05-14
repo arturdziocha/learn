1f21518b994c83f5d42dc58dbe2908ff40369759
arturdziocha <artur.dziocha@gmail.com>
2020-05-14 16:45:53 +0200

    ss

--- src/main/java/hyperskill/projects/linear/stage3/Main.java
+++ src/main/java/hyperskill/projects/linear/stage3/Main.java
@@ -1,38 +1,59 @@
 package hyperskill.projects.linear.stage3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Matrix matrix = new Matrix();
-        matrix.read(args[1]);
+        matrix.read("src/main/java/hyperskill/projects/linear/stage3/in.txt");
+        matrix.print();
     }
 }
 
 class Matrix {
-    private double[][] rows;
+    private Row[] matrix;
     private int n;
 
-    public void read(String file) {
-        Scanner scanner = new Scanner(file);
-        n = scanner.nextInt();
-        rows = new double[n][];
-        IntStream
-                .range(0, n)
-                .forEach(row -> rows[row] = Arrays
-                        .stream(scanner.nextLine().split(" "))
-                        .mapToDouble(Double::valueOf)
-                        .toArray());
+    public void read(String file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(file));
+        n = Integer.parseInt(scanner.nextLine());
+        
+        scanner.close();
+    }
+
+    void print() {
+        IntStream.range(0, n).forEach(System.out::println);
     }
 }
 
 class Row {
     private double[] row;
 
-    Row(double[] row) {
-        this.row = row;
+    Row(int size) {
+        this.row = new double[size];
+    }
+
+    void addValue(int num, double value) {
+        this.row[num] = value;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(row).boxed().map(String::valueOf).collect(Collectors.joining(" "));
+    }
+
+    public static Row parse(String string) {
+        double[] arr = Stream.of(string.split(" ")).mapToDouble(Double::valueOf).toArray();
+        Row row = new Row(arr.length);
+        IntStream.range(0, arr.length).forEach(i -> row.addValue(i, arr[i]));
+        return row;
+
     }
 
 }
