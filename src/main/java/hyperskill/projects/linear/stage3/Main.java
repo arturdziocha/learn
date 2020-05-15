9778bea718d9b8c6115c0b72fac62c4b87ba53b6
arturdziocha <artur.dziocha@gmail.com>
2020-05-15 16:43:05 +0200

    ss

--- src/main/java/hyperskill/projects/linear/stage3/Main.java
+++ src/main/java/hyperskill/projects/linear/stage3/Main.java
@@ -4,56 +4,63 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Matrix matrix = new Matrix();
         matrix.read("src/main/java/hyperskill/projects/linear/stage3/in.txt");
         matrix.print();
+        matrix.solve();
+        matrix.print();
     }
 }
 
 class Matrix {
-    private Row[] matrix;
+    private double[][] matrix;
     private int n;
 
     public void read(String file) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(file));
         n = Integer.parseInt(scanner.nextLine());
-        
+        matrix = new double[n][];
+        IntStream
+                .range(0, n)
+                .forEach(i -> matrix[i] = Arrays
+                        .stream(scanner.nextLine().split(" "))
+                        .mapToDouble(Double::valueOf)
+                        .toArray());
         scanner.close();
     }
 
     void print() {
-        IntStream.range(0, n).forEach(System.out::println);
-    }
-}
-
-class Row {
-    private double[] row;
-
-    Row(int size) {
-        this.row = new double[size];
+        IntStream.range(0, n).forEach(s -> System.out.println(Arrays.toString(matrix[s])));
     }
 
-    void addValue(int num, double value) {
-        this.row[num] = value;
+    void solve() {
+        System.out.println("Start solving the equation.");
+        if (matrix[0][0] != 1) {
+            double k = matrix[0][0];
+            IntStream.range(1, matrix[0].length).forEach(i -> matrix[0][i] /= k);
+            matrix[0][0] = 1;
+            System.out.printf("1 / %1.0f * R1 -> R1", k);
+            System.out.println();
+        }
+        IntStream.range(0, n).forEach(this::stage1);
     }
 
-    @Override
-    public String toString() {
-        return Arrays.stream(row).boxed().map(String::valueOf).collect(Collectors.joining(" "));
+    void stage1(int row) {
+        if (matrix[row][row] != 0) {
+            IntStream.range(row + 1, matrix[row].length).forEach(col -> matrix[row][col] /= matrix[row][row]);
+            System.out.println();
+            matrix[row][row] = 1;
+        }
+        print();
+        System.out.println();
+        IntStream.range(row + 1, n).forEach(i -> {
+            double k = matrix[i][row];
+            IntStream.range(row, n + 1).forEach(col -> matrix[i][col] -= k * matrix[row][col]);
+        });
+
     }
-
-    public static Row parse(String string) {
-        double[] arr = Stream.of(string.split(" ")).mapToDouble(Double::valueOf).toArray();
-        Row row = new Row(arr.length);
-        IntStream.range(0, arr.length).forEach(i -> row.addValue(i, arr[i]));
-        return row;
-
-    }
-
 }
