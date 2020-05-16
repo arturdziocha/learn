6a4f352d3b80418896b17e3512fad7232d3afa9e
arturdziocha <artur.dziocha@gmail.com>
2020-05-16 13:42:39 +0200

    ss

--- src/main/java/hyperskill/projects/linear/stage3/Main.java
+++ src/main/java/hyperskill/projects/linear/stage3/Main.java
@@ -2,16 +2,22 @@ package hyperskill.projects.linear.stage3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         Matrix matrix = new Matrix();
+        // matrix.read(args[1]);
         matrix.read("src/main/java/hyperskill/projects/linear/stage3/in.txt");
-        matrix.print();
+        // matrix.print();
         matrix.solve();
+        // matrix.write(args[3]);
+        matrix.write("src/main/java/hyperskill/projects/linear/stage3/out.txt");
     }
 }
 
@@ -19,8 +25,8 @@ class Matrix {
     private double[][] matrix;
     private int n;
 
-    public void read(String file) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File(file));
+    public void read(String fileName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
         n = Integer.parseInt(scanner.nextLine());
         matrix = new double[n][];
         IntStream
@@ -32,6 +38,19 @@ class Matrix {
         scanner.close();
     }
 
+    public void write(String filename) throws FileNotFoundException, IOException {
+        try (PrintWriter writer = new PrintWriter(new File(filename))) {
+            double[] toPrint = IntStream.range(0, n).mapToDouble(s -> matrix[s][n]).toArray();
+            System.out.println();
+            System.out
+                    .println("The solution is: ("
+                            + Arrays.stream(toPrint).boxed().map(String::valueOf).collect(Collectors.joining(", "))
+                            + ")");
+            Arrays.stream(toPrint).forEach(writer::println);
+            System.out.println("Saved to file " + filename);
+        }
+    }
+
     void print() {
         IntStream.range(0, n).forEach(s -> System.out.println(Arrays.toString(matrix[s])));
         System.out.println();
@@ -39,21 +58,9 @@ class Matrix {
 
     void solve() {
         System.out.println("Start solving the equation.");
-        if (matrix[0][0] != 1) {
-            double k = matrix[0][0];
-            IntStream.range(1, matrix[0].length).forEach(i -> matrix[0][i] /= k);
-            matrix[0][0] = 1;
-            System.out.printf("1 / %1.0f * R1 -> R1", k);
-            System.out.println();
-        }
-        print();
         IntStream.range(0, n).forEach(this::st1);
         for (int row = n - 1; row > 0; row--) {
-            for (int col = row; col > 0; col--) {
-                matrix[col][n] -= matrix[col][row]*matrix[row][n];
-            }
-            System.out.println(row);
-            print();
+            st2(row);
         }
     }
 
@@ -65,7 +72,7 @@ class Matrix {
             double k = matrix[row][row];
             IntStream.range(row, matrix[row].length).forEach(i -> matrix[row][i] /= k);
             System.out.println((1 / k) + " * R" + (row + 1) + " -> R" + (row + 1));
-            print();
+            // print();
         }
         IntStream.range(row + 1, n).forEach(i -> {
             double k = -matrix[i][row];
@@ -73,9 +80,19 @@ class Matrix {
                 matrix[i][j] += k * matrix[row][j];
             });
             System.out.println(k + " * R" + (row + 1) + " + R" + (i + 1) + " -> R" + (i + 1));
-            print();
+            // print();
         });
+    }
 
+    void st2(int row) {
+        for (int i = row - 1; i >= 0; i--) {
+            double k = -matrix[i][row];
+            for (int col = n; col >= 0; col--) {
+                matrix[i][col] += k * matrix[row][col];
+            }
+            System.out.println(k + " * R" + (row + 1) + " + R" + (i + 1) + " -> R" + (i + 1));
+            // print();
+        }
     }
 
     void stage1(int row) {
