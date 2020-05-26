43e90b9caa88bdc94ffe944f32fcb59378f21080
arturdziocha <artur.dziocha@gmail.com>
2020-05-26 23:03:26 +0200

    ss

--- src/main/java/hyperskill/projects/linear/stage5/ComplexNumber.java
+++ src/main/java/hyperskill/projects/linear/stage5/ComplexNumber.java
@@ -1,8 +1,6 @@
 package hyperskill.projects.linear.stage5;
 
 import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ComplexNumber {
     private double real;
@@ -23,62 +21,81 @@ public class ComplexNumber {
 
     @Override
     public String toString() {
-        return real + imaginary < 0 ? "" : "+" + imaginary + "i";
+        return "" + ((double) 0 == real ? "" : real) + ((double) 0 == imaginary ? "" : imaginary + "i");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ComplexNumber that = (ComplexNumber) o;
+
+        if (Double.compare(that.real, real) != 0) return false;
+        return Double.compare(that.imaginary, imaginary) == 0;
     }
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
+        int result;
         long temp;
-        temp = Double.doubleToLongBits(imaginary);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
         temp = Double.doubleToLongBits(real);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(imaginary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
         return result;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        ComplexNumber other = (ComplexNumber) obj;
-        if (Double.doubleToLongBits(imaginary) != Double.doubleToLongBits(other.imaginary))
-            return false;
-        if (Double.doubleToLongBits(real) != Double.doubleToLongBits(other.real))
-            return false;
-        return true;
-    }
-
     public static ComplexNumber parse(String number) {
-        String regex = "[-+]?(\\d+(\\.\\d*)?|\\.\\d+)";
-        Pattern doublePattern = Pattern.compile(regex);
-        Matcher matcher = doublePattern.matcher(number);
-        double r = 0.0;
-        double i = 0.0;
-        if (matcher.lookingAt()) {
-            r = Double.parseDouble(matcher.group());
-
-            number = number.substring(matcher.end());
-            matcher = doublePattern.matcher(number);
-            if (matcher.lookingAt()) {
-                i = Double.parseDouble(matcher.group());
-            } else {
-                i = 0.0;
-            }
+        boolean firstNegative = number.charAt(0) == '-';
+        boolean secondNegative = number.substring(1).contains("-");
+        String e;
+        if (firstNegative) {
+            e = number.substring(1);
+        } else {
+            e = number;
         }
-        System.out.println("real=" + r + ", imaginary=" + i);
-        String m = number.contains("-") ? "-" : "";
-        double[] numbers = Arrays
+        double[] values = Arrays.stream(e.split("[+-]")).mapToDouble(Double::valueOf).toArray();
+
+        System.out.println(firstNegative ? "-" : "" + number + " " + Arrays.toString(values));
+        return new ComplexNumber(values[0], 0);
+
+    }
+}
+/*
+double[] numbers = Arrays
                 .stream(number.split("[+-]"))
                 .mapToDouble(s -> s.isEmpty() ? Double.valueOf(0)
                         : s.contains("i") ? Double.valueOf(m + s.substring(0, s.length() - 1)) : Double.valueOf(s))
                 .toArray();
         // System.out.println(Arrays.toString(numbers));
         return new ComplexNumber(numbers[0], numbers.length == 1 ? numbers[1] : 0.0);
-    }
+ */
+
+/*
+boolean firstPositive = true;
+boolean secondPositive = true;
+if (s.charAt(0) == '-')     // See if first expr is negative
+    firstPositive = false;
+if (s.substring(1).contains("-"))
+    secondPositive = false;
+String[] split = s.split("[+-]");
+if (split[0].equals("")) {  // Handle expr beginning with `-`
+    split[0] = split[1];
+    split[1] = split[2];
 }
+double realPart = 0;
+double imgPart = 0;
+if (split[0].contains("i")) // Assumes input is not empty
+    imgPart = Double.parseDouble((firstPositive ? "+" : "-") + split[0].substring(0,split[0].length - 1));
+else
+    realPart = Double.parseDouble((firstPositive ? "+" : "-") + split[0]);
+if (split.length > 1) {     // Parse second part of expr if it exists
+    if (split[1].contains("i"))
+        imgPart = Double.parseDouble((secondPositive ? "+" : "-") + split[1].substring(0,split[1].length - 1));
+    else
+        realPart = Double.parseDouble((secondPositive ? "+" : "-") + split[1]);
+}
+// Use realPart and imgPart ...
+System.out.println(realPart + (imgPart < 0 ? "" : "+") + imgPart + "i");
+ */
--- src/test/java/hyperskill/projects/linear/stage5/ComplexNumberTest.java
+++ src/test/java/hyperskill/projects/linear/stage5/ComplexNumberTest.java
@@ -1,18 +1,51 @@
 package hyperskill.projects.linear.stage5;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ComplexNumberTest {
 
-    @BeforeEach
-    void setUp() throws Exception {}
-
     @Test
-    void test() {
-        fail("Not yet implemented");
+    @DisplayName("Parse should return Complex 0 0")
+    void firstTest() {
+        //Given
+        String string = "0";
+        //When
+        ComplexNumber complexNumber = ComplexNumber.parse(string);
+        //Then
+        Assertions.assertEquals(complexNumber, new ComplexNumber(0, 0));
+    }
+    @Test
+    @DisplayName("Given real 1 should return Complex{real=1, imaginary=0}")
+    void secondTest() {
+        //Given
+        String string = "1";
+        //When
+        ComplexNumber complexNumber = ComplexNumber.parse(string);
+        //Then
+        Assertions.assertEquals(complexNumber, new ComplexNumber(1, 0));
+    }
+    @Test
+    @DisplayName("Given real -1 should return Complex{real=-1, imaginary=0}")
+    void thirdTest() {
+        //Given
+        String string = "-1";
+        //When
+        ComplexNumber complexNumber = ComplexNumber.parse(string);
+        //Then
+        Assertions.assertEquals(new ComplexNumber(-1, 0), complexNumber);
+    }
+    @Test
+    @DisplayName("Given real 1.01 should return Complex{real=-1.01, imaginary=0}")
+    void fourTest() {
+        //Given
+        String string = "1.01";
+        //When
+        ComplexNumber complexNumber = ComplexNumber.parse(string);
+        //Then
+        Assertions.assertEquals(complexNumber, new ComplexNumber(1.01, 0));
     }
 
 }
